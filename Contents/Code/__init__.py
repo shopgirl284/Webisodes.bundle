749b0ad7b8a672fcf89811202421e908aaf45879
shopgirl284 <shopgirl284@gmail.com>
2013-04-22 22:08:06 -0400

    Added Channel
    Added Channel, updated icon and cleaned up the code

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -4,32 +4,13 @@ PREFIX   = '/video/webisodes'
 ART      = 'art-default.jpg'
 ICON     = 'icon-default.png'
 
-# For Web Therapy, we can use the main show ('http://www.lstudio.com/web-therapy/') or the RSS Feed ('http://www.lstudio.com/rss/lstudiorss-web-therapy.xml')
-WebTherapyURL = 'http://www.lstudio.com/rss/lstudiorss-web-therapy.xml'
-WebTherapyThumb = 'http://www.lstudio.com/img/WebTherapy_Series_197x111.jpg'
-
-# For Neil's Puppet Dreams, we can use the main page('http://henson-alternative.wikia.com/wiki/Neil%27s_Puppet_Dreams') or the YouTube playlist ('https://www.youtube.com/playlist?list=PLl4T6p7km9dbx0o8J35KjWAwaiEo5tV7G')
-# alternate for thumb is tvdb at 'http://thetvdb.com/banners/fanart/original/264624-2.jpg' show link is 'http://thetvdb.com/?tab=series&id=264624&lid=7'
-NeilSHOWURL = 'http://henson-alternative.wikia.com/wiki/Neil%27s_Puppet_Dreams'
-NeilSHOWPL = 'PLl4T6p7km9dbx0o8J35KjWAwaiEo5tV7G'
-NeilSHOWThumb = 'http://images.wikia.com/henson-alternative/images/4/46/NeilsPuppetDreams.jpg'
-
-# For Chris Hardwick's All Star Celebrity Bowling, we can use his page at Nerdist ('http://www.nerdist.com/category/bowling/') or the YouTube playlist ('http://www.youtube.com/playlist?list=PL4F5986B34F73F112')
-# Getting thumb from TVDB, show link is  'http://thetvdb.com/?tab=series&id=264502&lid=7'
-AllStarBowlingSHOWURL = 'http://www.nerdist.com/category/bowling/'
-AllStarBowlingSHOWPL = 'PL4F5986B34F73F112'
-AllStarBowlingSHOWThumb = 'http://thetvdb.com/banners/fanart/original/264502-1.jpg'
-
-# For Write Now! with Jimmy Pardo, we can use the nerdist page ('http://www.nerdist.com/2012/10/jimmy-pardos-new-show-starts-write-now/') or the YouTube playlist ('http://www.youtube.com/playlist?list=PLl4T6p7km9dYIjdVhuqlEFa4eP0kM3TQB')
-# Got thumb from YouTube
-WriteNowSHOWURL = 'http://www.youtube.com/playlist?list=PLl4T6p7km9dYIjdVhuqlEFa4eP0kM3TQB'
-WriteNowSHOWPL = 'PLl4T6p7km9dYIjdVhuqlEFa4eP0kM3TQB'
-WriteNowSHOWThumb = 'http://i2.ytimg.com/vi/am0dlMGN2nw/mqdefault.jpg'
-
 playlistURL = 'http://www.youtube.com/playlist?list='
-channelURL1 = 'http://www.youtube.com'
-channelURL2 = 'http://www.lstudio.com'
+YouTubeURL = 'http://www.youtube.com'
+LstudioURL = 'http://www.lstudio.com'
 channelURL3 = 'http://www.blip.tv'
+HuluURL = 'http://www.hulu.com'
+YahooURL = 'http://screen.yahoo.com'
+
 http = 'http:'
 
 ###################################################################################################
@@ -54,64 +35,67 @@ def Start():
 @handler(PREFIX, TITLE, art=ART, thumb=ICON)
 
 def MainMenu():
-
-# Since these shows will be added individually, this Main Menu will need to be setup differently
-# probably shouwl use RSS Feeds where available to keep it simple or youtube playlist where available
-
-# Could set up an xpath loop to go to each main site and pull info such as thumb and description
-
+# Since these shows will be added individually, for now this Main Menu will be hardcoded in.
+# would like to eventually pull this info from a database and even later have the option of entering info to add you own shows
   oc = ObjectContainer()
+
+  oc.add(DirectoryObject(
+    key=Callback(ShowRSS, title='Web Therapy', url='http://www.lstudio.com/rss/lstudiorss-web-therapy.xml'),
+    title="Web Therapy", 
+    thumb='http://www.lstudio.com/img/WebTherapy_Series_197x111.jpg'))
 	
   oc.add(DirectoryObject(
-    key=Callback(ShowRSS, title='Web Therapy', url=WebTherapyURL),
-    title="Web Therapy", 
-    thumb=WebTherapyThumb))
-		
+    key=Callback(ShowHulu, title="Battleground", url='battleground'), 
+    title="Battleground", 
+    thumb='http://ib2.huluim.com/show/8845?region=US&size=600x400'))
+	
   oc.add(DirectoryObject(
-    key=Callback(ShowPlaylist, title="Neil's Puppet Dreams", url=playlistURL + NeilSHOWPL), 
+    key=Callback(ShowYahoo, title="Burning Love", url='burning-love'), 
+    title="Burning Love", 
+    thumb='http://l.yimg.com/bt/api/res/1.2/6A3u9oiAMdXRTR8fdMdrKQ--/YXBwaWQ9eW5ld3M7Zmk9ZmlsbDtoPTEyOTtweW9mZj0wO3E9ODU7dz0yMzA-/http://l.yimg.com/os/595/2012/05/02/burninglovelogo-jpg_144203.jpg'))
+
+  oc.add(DirectoryObject(
+    key=Callback(ShowPlaylist, title="Neil's Puppet Dreams", url='PLl4T6p7km9dbx0o8J35KjWAwaiEo5tV7G'), 
     title="Neil's Puppet Dreams", 
-    thumb=NeilSHOWThumb))
+    thumb='https://i3.ytimg.com/vi/zBh__ymaFQo/hqdefault.jpg'))
 
   oc.add(DirectoryObject(
-    key=Callback(ShowPlaylist, title="Chris Hardwick's All Star Celebrity Bowling", url=playlistURL + AllStarBowlingSHOWPL), 
+    key=Callback(ShowPlaylist, title="Chris Hardwick's All Star Celebrity Bowling", url='PL4F5986B34F73F112'), 
     title="Chris Hardwick's All Star Celebrity Bowling", 
-    thumb=AllStarBowlingSHOWThumb))
+    thumb='http://i2.ytimg.com/vi/5M-iiIpzEfU/hqdefault.jpg'))
 
   oc.add(DirectoryObject(
-    key=Callback(ShowPlaylist, title="Write Now! with Jimmy Pardo", url=playlistURL + WriteNowSHOWPL), 
+    key=Callback(ShowPlaylist, title="Write Now! with Jimmy Pardo", url='PLl4T6p7km9dYIjdVhuqlEFa4eP0kM3TQB'), 
     title="Write Now! with Jimmy Pardo", 
-    thumb=WriteNowSHOWThumb))
+    thumb='http://i2.ytimg.com/vi/am0dlMGN2nw/mqdefault.jpg'))
 
   return oc
-
 ###################################################################################################
 @route(PREFIX + '/showplaylist')
 def ShowPlaylist(title, url):
 # This is for shows that have a YouTube Playlist. Hope it translates to all.
 
   oc = ObjectContainer()
+  url = playlistURL + url
 
   for video in HTML.ElementFromURL(url).xpath('//div[@id="gh-activityfeed"]/ol/li/div'):
 
-    url = video.xpath('./div/div/h3[@class="video-title-container"]/a//@href')[0]
-    url = channelURL1 + url
-    description = video.xpath('./div/div/p[@class="video-description yt-ui-ellipsis yt-ui-ellipsis-2"]//text()')[0]
-    Log('the value of description is%s'%description)
-    description = description.lstrip()
-    Log('the value of description is%s'%description)
-    title = video.xpath('./div/div/h3[@class="video-title-container"]/a//@title')[0]
-    duration = video.xpath('./div/div/span[@class="video-time"]//text()')[0]
-    duration = Datetime.MillisecondsFromString(duration)
-    Log('the value of duration is%s'%duration)
-    thumb = video.xpath('./div/div/span/span/span/span[@class="yt-thumb-clip-inner"]/img//@src') [0]
-    thumb = http + thumb
+    ep_url = video.xpath('./div/div/h3[@class="video-title-container"]/a//@href')[0]
+    ep_url = YouTubeURL + ep_url
+    ep_description = video.xpath('./div/div/p[@class="video-description yt-ui-ellipsis yt-ui-ellipsis-2"]//text()')[0]
+    ep_description = ep_description.lstrip()
+    ep_title = video.xpath('./div/div/h3[@class="video-title-container"]/a//@title')[0]
+    ep_duration = video.xpath('./div/div/span[@class="video-time"]//text()')[0]
+    ep_duration = Datetime.MillisecondsFromString(ep_duration)
+    ep_thumb = video.xpath('./div/div/span/span/span/span[@class="yt-thumb-clip-inner"]/img//@src') [0]
+    ep_thumb = http + ep_thumb
 	
     oc.add(VideoClipObject(
-      url = url, 
-      summary = description,
-      title = title, 
-      duration = duration,
-      thumb = Function(Thumb, url=thumb),
+      url = ep_url, 
+      summary = ep_description,
+      title = ep_title, 
+      duration = ep_duration,
+      thumb = Function(Thumb, url=ep_thumb),
       ))
 	  
   # it will loop through and return the values for all items in the page 
@@ -120,7 +104,7 @@ def ShowPlaylist(title, url):
 ########################################################################################################################
 @route(PREFIX + '/showrss')
 def ShowRSS(title, url):
-# This is for shows that have an RSS Feed.  Will have to look back to see if it will work with all
+# This is for shows that have an RSS Feed.  Will have to look back to see if it will work with all RSS feeds
 
   oc = ObjectContainer(title2=title)
   xml = RSS.FeedFromURL(url)
@@ -131,27 +115,17 @@ def ShowRSS(title, url):
     epDate = Datetime.ParseDate(item.date)
     # The description actually contains pubdate, link with thumb and description so we need to break it up
     epDesc = item.description
-    Log('the value of epDesc is%s'%epDesc)
     html = HTML.ElementFromString(epDesc)
-    Log('the value of html is%s'%html)
     # episode description are on line [2], trailers are line [1]. So need to figure out how to pull it
     els = list(html)
-    Log('the value of els is%s'%els)
     epThumb = html.cssselect('img')[0].get('src')
-    Log('the value of epThumb is%s'%epThumb)
 
     epSummary = []
-    Log('the value of epSummary is%s'%epSummary)
 
     for el in els:
       if el.tail: epSummary.append(el.tail)
-      Log('the value of el is%s'%el)
-      Log('the value of epSummary is%s'%epSummary)
 	
-    Log('the value of epSummary is%s'%epSummary)
-
     epSummary = '. '.join(epSummary)
-    Log('the value of epSummary is%s'%epSummary)
 	
     oc.add(VideoClipObject(
       url = epUrl, 
@@ -161,6 +135,86 @@ def ShowRSS(title, url):
       originally_available_at = epDate
       ))
 
+  return oc
+
+###################################################################################################
+@route(PREFIX + '/showhulu')
+def ShowHulu(title, url):
+# This is for shows that are on Hulu. 
+# cannot pull this data out for some reason says no results
+  url = HuluURL + '/grid/' + url + '?video_type=episode'
+
+  oc = ObjectContainer()
+  
+  for video in HTML.ElementFromURL(url).xpath('//div[@id="main"]/div/div/div/div/div/div[@id="episodes_8845_battleground_true"]/div/div/div/div/a'):
+
+    ep_url = video.xpath('.//@href')[0]
+    ep_url = HuluURL + ep_url
+    Log('the value of ep_url is%s'%ep_url)
+    # There is no description for these videos, just the season and episode number, so putting that in the description field
+    ep_description = video.xpath('./div/div[@class="title"]//text()')[0]
+    Log('the value of ep_description is%s'%ep_description)
+    ep_title = video.xpath('./div/div[@class="subtitle"]')[0].text()
+    ep_duration = video.xpath('./div/div/span[@class="duration"]//text()')[0]
+    # duration in in the format of ([number value] min) so need to convert it, if not a command then multiply times 60000
+    ep_duration = int(ep_duration.strip('(' , ' min)')) * 60000
+    # duration = Datetime.MillisecondsFromString(duration)
+    Log('the value of ep_duration is%s'%ep_duration)
+    ep_thumb = video.xpath('./img//@src') [0]
+	
+    oc.add(VideoClipObject(
+      url = ep_url, 
+      summary = ep_description,
+      title = ep_title, 
+      # duration = ep_duration,
+      thumb = Function(Thumb, url=ep_thumb),
+      ))
+	  
+  # it will loop through and return the values for all items in the page 
+  return oc
+  
+###################################################################################################
+@route(PREFIX + '/showyahoo')
+def ShowYahoo(title, url):
+
+  oc = ObjectContainer()
+  url = YahooURL + '/' + url
+  html = HTML.ElementFromURL(url)
+  
+  for video in html.xpath('//div[@class="feature"]/div/div[@class="bd"]/div'):
+
+    ep_url = video.xpath('./h2//@href')[0]
+    ep_url = YahooURL + ep_url
+    Log('the value of ep_url is%s'%ep_url)
+    # There is a description for these videos
+    ep_description = video.xpath('./p//text()')[0]
+    ep_title = video.xpath('./h2//text()')[0]
+    # There is no image or duration for these videos, so not adding these fields
+	
+    oc.add(VideoClipObject(
+      url = ep_url, 
+      title = ep_title, 
+      summary = ep_description
+      ))
+  
+  for video in html.xpath('//div/ul/li/ul/li[@data-provider-id="video.burninglove.com"]/div[@class="item-wrap"]'):
+
+    ep_url = video.xpath('./div/p[@class="title"]/a//@href')[0]
+    ep_url = YahooURL + ep_url
+    Log('the value of ep_url is%s'%ep_url)
+    # There is no description for these videos, just the title and episode number, so not adding the description field
+    ep_title = video.xpath('./div/p[@class="title"]/a//text()')[0]
+    # There is no duration for these videos, so not adding the duration field
+    ep_thumb = video.xpath('./a[@class="img-wrap"]/img') [0].get('src')
+    Log('the value of ep_thumb is%s'%ep_thumb)
+	
+    oc.add(VideoClipObject(
+      url = ep_url, 
+      title = ep_title, 
+      thumb = Function(Thumb, url=ep_thumb)
+      ))
+	  
+  # it will loop through and return the values for all items in the page 
   return oc
   
 ###########################################################################################################
